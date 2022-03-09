f2b45c1ccace87a9fef04d0a34e6499d053b9f68
ryker-dev <pizzuti.dev@gmail.com>
2022-03-09 18:26:52 +0200

    WIP base for app

--- client.py
+++ client.py
@@ -1,25 +1,105 @@
+## Base for code taken from Tech With Tim
+
 import socket
+import pickle
+import re
+import threading
+import os
 
 HEADERLENGTH = 8
 PORT = 5000
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDRESS = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "/disconnect"
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDRESS)
+def ask_name():
+    user = input("Username: ")
+    return user
 
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADERLENGTH - len(send_length))
-    client.send(send_length)
-    client.send(message)
-    print(client.recv(2048).decode(FORMAT))
+def ask_ip():
+    reg = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+    ip = input("Server IP:")
+    ip = re.search(reg, ip)
+    if (ip):
+        ip = ip.string()
+    else:
+        ip = socket.gethostbyname(socket.gethostname())
 
-send("Hello World!")
-input()
+    reg = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+    port = input("Server port:")
+    port = re.search(reg, port)
+    
+    if (port):
+        port.string()
+    else:
+        port = PORT
 
-send(DISCONNECT_MESSAGE)
+    return (ip, port)
+
+def connect():
+    address = ask_ip()
+    print("Connecting to server...")
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(address)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        return client
+    except:
+        print("CONNECTION ERROR")
+
+def disconnect(client, user):
+    '''     msg_length = client.recv(HEADERLENGTH).decode(FORMAT)
+    if (msg_length):
+        msg_length = int(msg_length)
+
+    data = client.recv(msg_length)
+    p = pickle.loads(data)
+
+    print(p["msg"]) '''
+
+    client.close()
+
+def send(client, user, message):
+    p = {"username": user, "msg": message }
+    p = pickle.dumps(p)
+
+    msg_length = len(p)
+    msg = bytes(f'{msg_length:<{HEADERLENGTH}}', FORMAT) + p
+
+    client.send(msg)
+
+    if (message == DISCONNECT_MESSAGE):
+        disconnect(client, user)
+
+    ## print(client.recv(2048).decode(FORMAT)) ##Debug print
+
+def receive(socket):
+    try:
+        msg_length = socket.recv(HEADERLENGTH).decode(FORMAT)
+        if (msg_length):
+            msg_length = int(msg_length)
+
+            data = socket.recv(msg_length)
+            p = pickle.loads(data)
+
+            username = p["username"]
+            msg = p["msg"]
+    except socket.error:
+        pass
+
+def chat_handler(socket):
+    while True:
+        receive(socket)
+
+def start():
+    user = ask_name()
+    socket = connect()
+
+    thread = threading.Thread(target=chat_handler, args=(socket,))
+    thread.start()
+
+    while socket:
+        msg = input(f"{user}: ")
+        send(socket, user, msg)
+    
+
+start()
--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 ## Base for code taken from Tech With Tim
 
+from http import client
 import socket
 import threading
+import pickle
 
 HEADERLENGTH = 8
 PORT = 5000
@@ -10,34 +12,68 @@ ADDRESS = (IP, PORT)
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "/disconnect"
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) ##IPV4 TCP
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) ##IPV4 TCP
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind(ADDRESS)
 
-s.bind(ADDRESS)
+SOCKET_LIST = [server]
+CLIENTS = []
 
-def client_thread(conn, addr):
-    print(f"[CONNECTION] Client {addr} connected")
+def send(client, user, message):
+    p = {"username": user, "msg": message }
+    p = pickle.dumps(p)
+    print(pickle.dumps(p))
 
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADERLENGTH).decode(FORMAT)
+    msg_length = len(p)
+    print(msg_length)
+    msg = bytes(f'{msg_length:<{HEADERLENGTH}}', FORMAT) + p
+
+    client.send(msg)
+
+def receive(client_socket):
+    try:
+        msg_length = client_socket.recv(HEADERLENGTH).decode(FORMAT)
         if (msg_length):
             msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-            print(f"[{addr}] {msg}")
-            conn.send("Msg received".encode(FORMAT))
 
-    conn.close()
+            data = client_socket.recv(msg_length)
+            p = pickle.loads(data)
+
+            if p["msg"] == DISCONNECT_MESSAGE:
+                return None
+            else:
+                return p
+                ##client_socket.send("Msg received".encode(FORMAT))
+    except socket.error:
+        #getpeername()
+        print(f"[CONNECTION] {client_socket.getpeername()[0]} disconnected unexpectedly")
+        return None
+
+def client_thread(client_socket, addr):
+    print(f"[CONNECTION] Client {addr[0]} connected")
+    print(f"[STATUS] Client connections: {threading.active_count() - 1}")
+
+    send(client_socket, "SERVER", "Welcome to the server!")
+    connected = True
+    while connected:
+        p = receive(client_socket)
+        if (not p):
+            connected = False
+        else:
+            print("{}: {}".format(p["username"], p["msg"]))
+    print(f"[CONNECTION] {client_socket.getpeername()[0]} disconnected")
+    client_socket.close()
 
 def start():
-    s.listen()
+    server.listen()
     print(f"[STARTING] Server listening on {IP}:{PORT}")
     while True:
-        conn, addr = s.accept()
-        thread = threading.Thread(target=client_thread, args=(conn, addr))
+        client_socket, addr = server.accept()
+        SOCKET_LIST.append(client_socket)
+        CLIENTS.append(client_socket)
+
+        thread = threading.Thread(target=client_thread, args=(client_socket, addr))
         thread.start()
-        print(f"[STATUS] Client connections: {threading.active_count() - 1}")
 
 print("[STARTING] Server is starting...")
 start()
